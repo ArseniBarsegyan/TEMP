cbbd178493772d374da861bfa9289a65fbe309dc
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-10 10:40:30 +0300

    Divide logic to CsvDbRecorder and Logger

---
+++ Task04/ManagerService/Classes/CsvDbRecorder.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using AutoMapper;
+using ManagerService.Models;
+using Task04.DAL.Context;
+using Task04.DAL.Entities;
+using Task04.DAL.Repositories;
+
+namespace ManagerService.Classes
+{
+    public class CsvDbRecorder
+    {
+        private string _nameOrConnectionString;
+        private object _lockObject = new object();
+        private GenericRepository<Manager> _managerRepo;
+
+        public CsvDbRecorder(string nameOrConnectionString)
+        {
+            _nameOrConnectionString = nameOrConnectionString;
+        }
+
+        public void WriteDataToDataBaseFromFile(string fileName)
+        {
+            _managerRepo = new GenericRepository<Manager>(new AppDbContext(_nameOrConnectionString));
+
+            var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileName);
+            if (fileNameWithoutExtension == null) return;
+            var splittedFileName = fileNameWithoutExtension.Split('_');
+            var managerName = splittedFileName[0];
+            var managerDate = splittedFileName[1];
+
+            var purchaseDto = new PurchaseDto()
+            {
+                ManagerName = managerName,
+                Date = DateTime.ParseExact(managerDate, "ddMMyyyy", System.Globalization.CultureInfo.InvariantCulture),
+            };
+
+            using (var reader = new StreamReader(fileName))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var lineContent = line.Split(',');
+                    purchaseDto.ClientName = lineContent[0].Trim();
+                    purchaseDto.ProductName = lineContent[1].Trim();
+                    purchaseDto.Price = decimal.Parse(lineContent[2].Trim());
+
+                    Mapper.Initialize(cfg =>
+                    {
+                        cfg.CreateMap<PurchaseDto, Product>()
+                            .ForMember(x => x.Name, opt => opt.MapFrom(src => src.ProductName))
+                            .ForMember(x => x.Date, opt => opt.MapFrom(src => src.Date))
+                            .ForMember(x => x.Client, opt => opt.MapFrom(src => src.ClientName))
+                            .ForMember(x => x.Price, opt => opt.MapFrom(src => src.Price));
+                        cfg.CreateMap<PurchaseDto, Manager>()
+                            .ForMember(x => x.LastName, opt => opt.MapFrom(src => src.ManagerName));
+                    });
+
+                    var product = Mapper.Map<PurchaseDto, Product>(purchaseDto);
+
+                    lock (_lockObject)
+                    {
+                        var manager = _managerRepo.GetAll().FirstOrDefault(x => x.LastName == purchaseDto.ManagerName);
+                        if (manager == null)
+                        {
+                            manager = Mapper.Map<PurchaseDto, Manager>(purchaseDto);
+                            manager.Products.Add(product);
+                            _managerRepo.Create(manager);
+                        }
+                        else
+                        {
+                            manager.Products.Add(product);
+                            _managerRepo.Update(manager);
+                        }
+
+                        _managerRepo.Save();
+                    }
+
+                }
+            }
+        }
+    }
+}
--- Task04/ManagerService/Classes/Logger.cs
+++ Task04/ManagerService/Classes/Logger.cs
@@ -1,28 +1,21 @@
-﻿using System;
-using System.Configuration;
+﻿using System.Configuration;
 using System.IO;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using AutoMapper;
-using ManagerService.Models;
-using Task04.DAL.Context;
-using Task04.DAL.Entities;
-using Task04.DAL.Repositories;
 
 namespace ManagerService.Classes
 {
     public class Logger
     {
         private readonly FileSystemWatcher _watcher;
-        object _obj = new object();
+        private object _obj = new object();
         private bool _enabled = true;
-        private GenericRepository<Manager> _managerRepo;
-        private GenericRepository<Product> _productRepo;
+        private CsvDbRecorder _dbRecorder;
 
         public Logger()
         {
             _watcher = new FileSystemWatcher(ConfigurationManager.AppSettings["ServerPath"]);
+            _dbRecorder = new CsvDbRecorder("DefaultConnection");
 
             _watcher.Created += WatcherOnCreated;
             _watcher.Changed += WatcherOnChanged;
@@ -54,7 +47,7 @@ namespace ManagerService.Classes
 
             var task = new Task(() =>
             {
-                WriteDataToDataBaseFromFile(fileSystemEventArgs.FullPath);
+                _dbRecorder.WriteDataToDataBaseFromFile(fileSystemEventArgs.FullPath);
             });
             task.Start();
             task.Wait();
@@ -68,63 +61,6 @@ namespace ManagerService.Classes
                 {
                     writer.WriteLine($"{filePath} was {fileEvent}");
                     writer.Flush();
-                }
-            }
-        }
-
-        private void WriteDataToDataBaseFromFile(string fileName)
-        {
-            _managerRepo = new GenericRepository<Manager>(new AppDbContext("DefaultConnection"));
-
-            var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileName);
-            if (fileNameWithoutExtension == null) return;
-            var splittedFileName = fileNameWithoutExtension.Split('_');
-            var managerName = splittedFileName[0];
-            var managerDate = splittedFileName[1];
-
-            var purchaseDto = new PurchaseDto()
-            {
-                ManagerName = managerName,
-                Date = DateTime.ParseExact(managerDate, "ddMMyyyy", System.Globalization.CultureInfo.InvariantCulture),
-            };
-
-            using (var reader = new StreamReader(fileName))
-            {
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                {
-                    var lineContent = line.Split(',');
-                    purchaseDto.ClientName = lineContent[0].Trim();
-                    purchaseDto.ProductName = lineContent[1].Trim();
-                    purchaseDto.Price = decimal.Parse(lineContent[2].Trim());
-
-                    Mapper.Initialize(cfg =>
-                    {
-                        cfg.CreateMap<PurchaseDto, Product>()
-                            .ForMember(x => x.Name, opt => opt.MapFrom(src => src.ProductName))
-                            .ForMember(x => x.Date, opt => opt.MapFrom(src => src.Date))
-                            .ForMember(x => x.Client, opt => opt.MapFrom(src => src.ClientName))
-                            .ForMember(x => x.Price, opt => opt.MapFrom(src => src.Price));
-                        cfg.CreateMap<PurchaseDto, Manager>()
-                            .ForMember(x => x.LastName, opt => opt.MapFrom(src => src.ManagerName));
-                    });
-
-                    var product = Mapper.Map<PurchaseDto, Product>(purchaseDto);
-
-                    var manager = _managerRepo.GetAll().FirstOrDefault(x => x.LastName == purchaseDto.ManagerName);
-                    if (manager == null)
-                    {
-                        manager = Mapper.Map<PurchaseDto, Manager>(purchaseDto);
-                        manager.Products.Add(product);
-                        _managerRepo.Create(manager);
-                    }
-                    else
-                    {
-                        manager.Products.Add(product);
-                        _managerRepo.Update(manager);
-                    }
-
-                    _managerRepo.Save();
                 }
             }
         }
