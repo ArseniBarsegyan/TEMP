d35154cef5272890cc251252232600b874256197
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-14 09:45:31 +0300

    Added Task05. Added DAL. Added Entities: Client, Manager, Product

---
+++ Task05/ManagerSystem.DAL/Entities/Client.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ManagerSystem.DAL.Entities
+{
+    public class Client
+    {
+        public Client()
+        {
+            Products = new List<Product>();
+        }
+
+        public string Name { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ Task05/ManagerSystem.DAL/Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ManagerSystem.DAL.Entities
+{
+    public class Product : Entity
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Price { get; set; }
+        public virtual Manager Manager { get; set; }
+        public virtual Client Client { get; set; }
+    }
+}
