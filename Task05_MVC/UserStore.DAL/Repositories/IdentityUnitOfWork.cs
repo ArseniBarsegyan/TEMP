5e83cf9ab38e1b23a4a24154e9cd6fd81e85bfac
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-17 11:23:15 +0300

    DAL: added IdentityUnitOfWork

---
+++ Task05_MVC/UserStore.DAL/Repositories/IdentityUnitOfWork.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+using UserStore.DAL.EF;
+using UserStore.DAL.Entities;
+using UserStore.DAL.Identity;
+using UserStore.DAL.Interfaces;
+
+namespace UserStore.DAL.Repositories
+{
+    public class IdentityUnitOfWork : IUnitOfWork
+    {
+        private ApplicationContext _context;
+        private bool _disposed;
+
+        public IdentityUnitOfWork(string connectionString)
+        {
+            _context = new ApplicationContext(connectionString);
+            UserManager = new ApplicationUserManager(new UserStore<ApplicationUser>(_context));
+            RoleManager = new ApplicationRoleManager(new RoleStore<ApplicationRole>(_context));
+        }
+
+        public ApplicationUserManager UserManager { get; }
+        public ApplicationRoleManager RoleManager { get; }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (_disposed) return;
+            if (disposing)
+            {
+                UserManager.Dispose();
+                RoleManager.Dispose();
+            }
+            _disposed = true;
+        }
+    }
+}
