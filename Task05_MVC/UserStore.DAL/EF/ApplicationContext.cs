63189737787234e667d86cc9746eafce8100a774
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-17 11:16:42 +0300

    DAL: added ApplicationContext

---
+++ Task05_MVC/UserStore.DAL/EF/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using UserStore.DAL.Entities;
+
+namespace UserStore.DAL.EF
+{
+    public class ApplicationContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationContext(string connectionString)
+            : base(connectionString)
+        {
+
+        }
+    }
+}
