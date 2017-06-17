7005df713f4e5bcf408146f7c57c80555b4823b3
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-17 11:36:47 +0300

    BLL: added interface IUserService

---
+++ Task05_MVC/UserStore.BLL/Interfaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using UserStore.BLL.DTO;
+using UserStore.BLL.Infrastructure;
+
+namespace UserStore.BLL.Interfaces
+{
+    public interface IUserService : IDisposable
+    {
+        Task<OperationDetails> CreateAsync(UserDto userDto);
+        Task<ClaimsIdentity> Authenticate(UserDto userDto);
+        Task SetInitialData(UserDto adminDto, IEnumerable<string> roles);
+    }
+}
