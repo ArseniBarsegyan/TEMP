bbd8fcf9afe65e8aafd7b15e0b6345aee49437db
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-17 12:39:55 +0300

    AccountController: adde Login() method

--- Task05_MVC/UserStore.WebUI/Controllers/AccountController.cs
+++ Task05_MVC/UserStore.WebUI/Controllers/AccountController.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
+using UserStore.BLL.DTO;
 using UserStore.BLL.Interfaces;
+using UserStore.WebUI.Models;
 
 namespace UserStore.WebUI.Controllers
 {
@@ -14,10 +17,34 @@ namespace UserStore.WebUI.Controllers
         private IUserService UserService => HttpContext.GetOwinContext().GetUserManager<IUserService>();
         private IAuthenticationManager AuthenticationManager => HttpContext.GetOwinContext().Authentication;
 
-        // GET: Account
-        public ActionResult Index()
+        public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var userDto = new UserDto {Name = model.Name, Password = model.Password, Role = "user"};
+                var claim = await UserService.Authenticate(userDto);
+                if (claim == null)
+                {
+                    ModelState.AddModelError("", "Incorrect login or password");
+                }
+                else
+                {
+                    AuthenticationManager.SignOut();
+                    AuthenticationManager.SignIn(new AuthenticationProperties
+                    {
+                        IsPersistent = true
+                    }, claim);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View(model);
         }
     }
 }
