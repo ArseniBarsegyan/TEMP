059f3fdd1468b2e2a6686abe65946ec0a378170d
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-17 11:50:29 +0300

    WebUI: added class Startup for OWIN

---
+++ Task05_MVC/UserStore.WebUI/App_Start/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using UserStore.BLL.Interfaces;
+using UserStore.BLL.Services;
+
+[assembly: OwinStartup(typeof(UserStore.WebUI.Startup))]
+namespace UserStore.WebUI
+{
+    public class Startup
+    {
+        readonly IServiceCreator _serviceCreator = new ServiceCreator();
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext(CreateUserService);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+            });
+        }
+
+        private IUserService CreateUserService()
+        {
+            return _serviceCreator.CreateUserService("DefaultConnection");
+        }
+    }
+}
