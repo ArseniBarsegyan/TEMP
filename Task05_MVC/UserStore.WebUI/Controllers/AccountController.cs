8743f89faf78df1bdbdc9a81265f31608926f314
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-17 12:14:48 +0300

    WebUI: Added AccountController

---
+++ Task05_MVC/UserStore.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+using UserStore.BLL.Interfaces;
+
+namespace UserStore.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private IUserService UserService => HttpContext.GetOwinContext().GetUserManager<IUserService>();
+
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
