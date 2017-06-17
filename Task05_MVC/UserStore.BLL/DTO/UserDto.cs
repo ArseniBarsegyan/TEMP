891e345b15f49cfce8503e16a789259d6534050e
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-17 11:26:56 +0300

    BLL: added UserDto

---
+++ Task05_MVC/UserStore.BLL/DTO/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace UserStore.BLL.DTO
+{
+    public class UserDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
