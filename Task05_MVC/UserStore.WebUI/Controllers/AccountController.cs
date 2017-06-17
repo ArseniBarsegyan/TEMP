5015ac5e986e6a8a9a276676e9ebf3412cfb931c
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-17 12:17:23 +0300

    WebUI: AccountController: added IAuthenticationManager (get from OWIN context)

--- Task05_MVC/UserStore.WebUI/Controllers/AccountController.cs
+++ Task05_MVC/UserStore.WebUI/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
 using UserStore.BLL.Interfaces;
 
 namespace UserStore.WebUI.Controllers
@@ -11,6 +12,7 @@ namespace UserStore.WebUI.Controllers
     public class AccountController : Controller
     {
         private IUserService UserService => HttpContext.GetOwinContext().GetUserManager<IUserService>();
+        private IAuthenticationManager AuthenticationManager => HttpContext.GetOwinContext().Authentication;
 
         // GET: Account
         public ActionResult Index()
