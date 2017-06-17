cfc60925b52e2050bfdf4712d3f0dc34d7547c2f
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-17 18:06:44 +0300

    WebUI: added AdminController (can register other users)

---
+++ Task05_MVC/UserStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using UserStore.BLL.DTO;
+using UserStore.BLL.Interfaces;
+using UserStore.WebUI.Models;
+
+namespace UserStore.WebUI.Controllers
+{
+    //Admin controls all users creation
+    //and can manipulate them and sell data
+    [Authorize(Roles = "admin")]
+    public class AdminController : Controller
+    {
+        private IUserService UserService => HttpContext.GetOwinContext().GetUserManager<IUserService>();
+        private IAuthenticationManager AuthenticationManager => HttpContext.GetOwinContext().Authentication;
+
+        public ActionResult Index()
+        {
+            return View(UserService.GetAllUsersList());
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var userDto = new UserDto
+                {
+                    Name = model.Name,
+                    Password = model.Password,
+                    Role = "user"
+                };
+
+                var operationDetails = await UserService.CreateAsync(userDto);
+
+                if (operationDetails.Succedeed)
+                    return RedirectToAction("Index", "Admin");
+                ModelState.AddModelError(operationDetails.Property, operationDetails.Message);
+            }
+            return View(model);
+        }
+    }
+}
