96168595c19a6b1ce009e23f802bbbca6fd118a7
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-17 11:13:44 +0300

    DAL: added ApplicationRoleManager, ApplicationUserManager

---
+++ Task05_MVC/UserStore.DAL/Identity/ApplicationRoleManager.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity;
+using UserStore.DAL.Entities;
+
+namespace UserStore.DAL.Identity
+{
+    public class ApplicationRoleManager : RoleManager<ApplicationRole>
+    {
+        public ApplicationRoleManager(IRoleStore<ApplicationRole, string> store) 
+            : base(store)
+        {
+
+        }
+    }
+}
---
+++ Task05_MVC/UserStore.DAL/Identity/ApplicationUserManager.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity;
+using UserStore.DAL.Entities;
+
+namespace UserStore.DAL.Identity
+{
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+        public ApplicationUserManager(IUserStore<ApplicationUser> store) 
+            : base(store)
+        {
+
+        }
+    }
+}
