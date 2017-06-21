11cc4f0540b25f5653a36dd670d7070316073b53
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-21 12:30:04 +0300

    BLL: Added IManagerService interface

---
+++ Task05_MVC/UserStore.BLL/Interfaces/IManagerService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UserStore.BLL.DTO;
+using UserStore.BLL.Infrastructure;
+
+namespace UserStore.BLL.Interfaces
+{
+    public interface IManagerService
+    {
+        IEnumerable<ManagerDto> GetAllManagersList();
+        IEnumerable<ManagerDto> GetManagerById(int id);
+        OperationDetails Create(ManagerDto managerDto);
+        OperationDetails Edit(ManagerDto managerDto);
+        OperationDetails Delete(int id);
+    }
+}
