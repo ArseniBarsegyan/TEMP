ee2923b0c7e6e5ec9366e3c310ac4022d23f9d35
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-21 12:49:27 +0300

    BLL: Added ManagerService. WebUI: Added ManagerController

---
+++ Task05_MVC/UserStore.BLL/Services/ManagerService.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using UserStore.BLL.DTO;
+using UserStore.BLL.Infrastructure;
+using UserStore.BLL.Interfaces;
+using UserStore.DAL.Repositories;
+
+namespace UserStore.BLL.Services
+{
+    public class ManagerService : IManagerService
+    {
+        private IdentityUnitOfWork UnitOfWork { get; }
+
+        public ManagerService(IdentityUnitOfWork unitOfWork)
+        {
+            UnitOfWork = unitOfWork;
+        }
+
+        public IEnumerable<ManagerDto> GetAllManagersList()
+        {
+            var managerList = new List<ManagerDto>();
+
+            foreach (var manager in UnitOfWork.ManagerRepository.GetAll())
+            {
+                var managerDto = new ManagerDto
+                {
+                    Id = manager.Id,
+                    LastName = manager.LastName
+                };
+                managerList.Add(managerDto);
+            }
+            return managerList;
+        }
+
+        public ManagerDto GetManagerById(int id)
+        {
+            var manager = UnitOfWork.ManagerRepository.GetById(id);
+            var managerDto = new ManagerDto
+            {
+                Id = manager.Id,
+                LastName = manager.LastName
+            };
+            return managerDto;
+        }
+
+        public OperationDetails Create(ManagerDto managerDto)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public OperationDetails Edit(ManagerDto managerDto)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public OperationDetails Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Task05_MVC/UserStore.WebUI/Controllers/ManagerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserStore.BLL.Interfaces;
+using UserStore.BLL.Services;
+using UserStore.DAL.Repositories;
+
+namespace UserStore.WebUI.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public class ManagerController : Controller
+    {
+        private IManagerService _managerService = new ManagerService(new IdentityUnitOfWork("DefaultConnection"));
+
+        public ActionResult Index()
+        {
+            var allManagers = _managerService.GetAllManagersList();
+            return View(allManagers);
+        }
+    }
+}
