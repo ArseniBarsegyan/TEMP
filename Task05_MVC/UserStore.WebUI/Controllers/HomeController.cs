4f331b714223cfda9d6a49a2965eade263dc99e4
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-21 20:23:30 +0300

    Little update. Added all orders to Index() of HomeController

--- Task05_MVC/UserStore.WebUI/Controllers/HomeController.cs
+++ Task05_MVC/UserStore.WebUI/Controllers/HomeController.cs
@@ -1,12 +1,18 @@
 ﻿using System.Web.Mvc;
+using UserStore.BLL.Interfaces;
+using UserStore.BLL.Services;
+using UserStore.DAL.Repositories;
 
 namespace UserStore.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        private IOrderService _orderService = new OrderService(new UnitOfWork("DefaultConnection"));
+
         public ActionResult Index()
         {
-            return View();
+            var allOrders = _orderService.GetAllOrderList();
+            return View(allOrders);
         }
     }
 }
