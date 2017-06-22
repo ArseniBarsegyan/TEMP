d6be42b7d26ca96d55ad868ec1a21bfe8b2bb026
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-06-22 22:35:47 +0300

    WebUI: almost done with filters. Only filter by price value left

--- Task05_MVC/UserStore.WebUI/Controllers/HomeController.cs
+++ Task05_MVC/UserStore.WebUI/Controllers/HomeController.cs
@@ -1,7 +1,11 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 using UserStore.BLL.Interfaces;
 using UserStore.BLL.Services;
 using UserStore.DAL.Repositories;
+using UserStore.WebUI.Models;
 
 namespace UserStore.WebUI.Controllers
 {
@@ -9,15 +13,59 @@ namespace UserStore.WebUI.Controllers
     {
         private IOrderService _orderService = new OrderService(new UnitOfWork("DefaultConnection"));
 
-        public ActionResult Index()
+        public ActionResult Index(string manager, string product, decimal? price, DateTime? date = null)
         {
-            return View();
+            var orders = _orderService.GetAllOrderList();
+
+            var managers = orders.Select(x => x.ManagerName).Distinct().ToList();
+            managers.Insert(0, "All");
+            var products = orders.Select(x => x.ProductName).Distinct().ToList();
+            products.Insert(0, "All");
+            var dates = orders.Select(x => x.Date).Distinct().ToList();
+            dates.Insert(0, DateTime.Today);
+            var prices = orders.Select(x => x.Price).Distinct().ToList();
+            prices.Insert(0, 0);
+
+            if (!string.IsNullOrEmpty(manager) && !manager.Equals("All"))
+            {
+                orders = orders.Where(x => x.ManagerName == manager);
+            }
+            if (!string.IsNullOrEmpty(product) && !product.Equals("All"))
+            {
+                orders = orders.Where(x => x.ProductName == product);
+            }
+
+            if (price != null && price != 0)
+            {
+                orders = orders.Where(x => x.Price == price);
+            }
+
+            if (!date.HasValue) 
+            {
+                orders = orders.Where(x => x.Date == DateTime.Today);
+            }
+
+            var ordersListViewModel = new OrderListViewModel
+            {
+                Orders = orders,
+                Managers = new SelectList(managers),
+                Products = new SelectList(products),
+                Dates = new SelectList(dates),
+                Prices = new SelectList(prices)
+            };
+
+            return View(ordersListViewModel);
         }
 
         [HttpPost]
         public ActionResult OrdersList()
         {
             var allOrders = _orderService.GetAllOrderList();
+            
+            ViewBag.Managers = allOrders.Select(order => order.ManagerName);
+            ViewBag.Products = allOrders.Select(order => order.ProductName);
+            ViewBag.Dates = allOrders.Select(order => order.Date);
+
             return PartialView(allOrders);
         }
     }
---
+++ Task05_MVC/UserStore.WebUI/Models/OrderListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using UserStore.BLL.DTO;
+
+namespace UserStore.WebUI.Models
+{
+    public class OrderListViewModel
+    {
+        public IEnumerable<OrderDto> Orders { get; set; }
+        public SelectList Managers { get; set; }
+        public SelectList Products { get; set; }
+        public SelectList Dates { get; set; }
+        public SelectList Prices { get; set; }
+    }
+}
