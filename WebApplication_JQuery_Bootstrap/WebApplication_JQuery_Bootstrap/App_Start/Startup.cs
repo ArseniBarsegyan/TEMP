b96b326dc59bf88562cbc97ff90d56b7e8f56f4d
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-07-21 13:51:15 +0300

    Example updated

---
+++ WebApplication_JQuery_Bootstrap/WebApplication_JQuery_Bootstrap/App_Start/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApplication_JQuery_Bootstrap.Startup))]
+namespace WebApplication_JQuery_Bootstrap
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+            });
+        }
+    }
+}
--- WebApplication_JQuery_Bootstrap/WebApplication_JQuery_Bootstrap/Controllers/HomeController.cs
+++ WebApplication_JQuery_Bootstrap/WebApplication_JQuery_Bootstrap/Controllers/HomeController.cs
@@ -15,8 +15,6 @@ namespace WebApplication_JQuery_Bootstrap.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
