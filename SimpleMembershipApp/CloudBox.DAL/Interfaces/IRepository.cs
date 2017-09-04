be3ca908db38fb42eb008ecee4d09417c31a6fb9
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-09-04 23:39:57 +0300

    Updated. Added DAL, TestDAL app

---
+++ SimpleMembershipApp/CloudBox.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CloudBox.DAL.Interfaces
+{
+    public interface IRepository
+    {
+        ICollection<string> GetAllFilesByUserId(int id);
+        void WriteDataToDataBase(int userId, string fileName, byte[] data, DateTime uploadTime);
+        void RenameFileInDataBase(int userId, string oldFileName, string newFileName);
+    }
+}
---
+++ SimpleMembershipApp/CloudBox.DAL/Repositories/Repository.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using CloudBox.DAL.Interfaces;
+using MySql.Data.MySqlClient;
+
+namespace CloudBox.DAL.Repositories
+{
+    //Provides CRUD operations
+    public class Repository : IRepository
+    {
+        private readonly string _connectionString;
+
+        public Repository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        //Open connection with database and get all file records
+        public ICollection<string> GetAllFilesByUserId(int id)
+        {
+            ICollection<string> resultList = new List<string>();
+            
+            using (MySqlConnection connection = new MySqlConnection(_connectionString))
+            {
+                connection.Open();
+                MySqlCommand command = new MySqlCommand("GetAllFilesById", connection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                command.Parameters.AddWithValue("@Id", id);
+
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    resultList.Add(reader[0] + " " + reader[1] + " " + reader[2] + " " + reader[3] + " " + reader[4]);
+                }
+                reader.Close();
+                return resultList;
+            }
+        }
+
+        //Open connection with database, write data and close connection
+        public void WriteDataToDataBase(int userId, string fileName, byte[] data, DateTime uploadTime)
+        {
+            using (MySqlConnection connection = new MySqlConnection(_connectionString))
+            {
+                connection.Open();
+                MySqlCommand command = new MySqlCommand("WriteDataToDataBase", connection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                command.Parameters.AddWithValue("@userID", userId);
+                command.Parameters.AddWithValue("@fileName", fileName);
+                command.Parameters.AddWithValue("@data", data);
+                command.Parameters.AddWithValue("@uploadDate", uploadTime);
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void RenameFileInDataBase(int userId, string oldFileName, string newFileName)
+        {
+            using (MySqlConnection connection = new MySqlConnection(_connectionString))
+            {
+                connection.Open();
+                MySqlCommand command = new MySqlCommand("RenameFileInDataBase", connection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                command.Parameters.AddWithValue("@userID", userId);
+                command.Parameters.AddWithValue("@oldName", oldFileName);
+                command.Parameters.AddWithValue("@newName", newFileName);
+
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
