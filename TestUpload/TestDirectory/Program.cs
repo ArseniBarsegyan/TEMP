8aa4986b7fb6a959b5347ec601e3720f2f046bb0
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-09-06 06:50:22 +0300

    Updated. Method works

---
+++ TestUpload/TestDirectory/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] subdirectoryEntries = Directory.GetDirectories(@"D:\1");
+            foreach (string directoryFullName in subdirectoryEntries)
+            {
+                string directoryName = new FileInfo(directoryFullName).Name;
+                Console.WriteLine(directoryName);
+            }
+        }
+    }
+}
--- TestUpload/TestUpload/Controllers/HomeController.cs
+++ TestUpload/TestUpload/Controllers/HomeController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace TestUpload.Controllers
@@ -11,8 +9,7 @@ namespace TestUpload.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Files = GetFilesInDirectory();
-            ViewBag.Directories = GetSubdirectoriesInDirectory();
+            ViewBag.Directories = GetAllDirectoriesByUserName("SuperMan");
             return View();
         }
 
@@ -30,33 +27,20 @@ namespace TestUpload.Controllers
             return View();
         }
 
-        [HttpPost]
-        public JsonResult Upload()
+        private IEnumerable<string> GetAllDirectoriesByUserName(string username)
         {
-            foreach (string file in Request.Files)
+            CheckIfDirectoryWithUserNameExists(username);
+            return Directory.GetDirectories(Server.MapPath("~/Files/" + username));
+        }
+
+        private void CheckIfDirectoryWithUserNameExists(string username)
+        {
+            var usersDirectories = Directory.GetDirectories(Server.MapPath("~/Files/"));
+            var directoriesNames = usersDirectories.Select(directoryFullName => new FileInfo(directoryFullName).Name);
+            if (!directoriesNames.Contains(username))
             {
-                var upload = Request.Files[file];
-                if (upload != null)
-                {
-                    // получаем имя файла
-                    string fileName = System.IO.Path.GetFileName(upload.FileName);
-                    // сохраняем файл в папку Files в проекте
-                    upload.SaveAs(Server.MapPath("~/Files/" + fileName));
-                }
+                Directory.CreateDirectory(Server.MapPath("~/Files/" + username));
             }
-            return Json("файл загружен");
-        }
-
-        private ICollection<string> GetFilesInDirectory()
-        {
-            string[] fileEntries = Directory.GetFiles(Server.MapPath("~/Files/"));
-            return fileEntries.ToList();
-        }
-
-        private ICollection<string> GetSubdirectoriesInDirectory()
-        {
-            string[] subdirectoryEntries = Directory.GetDirectories(Server.MapPath("~/Files/"));
-            return subdirectoryEntries.ToList();
         }
     }
 }
