6ae91370fe382535c726e509760b135e3ab0dfdb
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-09-06 16:53:01 +0300

    Project updated

--- TestUpload/TestUpload/Controllers/HomeController.cs
+++ TestUpload/TestUpload/Controllers/HomeController.cs
@@ -7,9 +7,14 @@ namespace TestUpload.Controllers
 {
     public class HomeController : Controller
     {
+        //Index содержит текущую директорию пользователя
+        //а также список все файлов в текущей директории
         public ActionResult Index()
         {
-            ViewBag.Directories = GetAllDirectoriesByUserName("SuperMan");
+            var currentFolders = GetAllDirectoriesByUserName("SuperMan");
+            ViewBag.CurrentPath = GetUserFolderRelativePath("SuperMan");
+            ViewBag.Directories = currentFolders;
+            ViewBag.Files = GetAllFilesByUserName("SuperMan");
             return View();
         }
 
@@ -27,12 +32,37 @@ namespace TestUpload.Controllers
             return View();
         }
 
+        //Получаем абсолютный путь из переданных папок
+        private string GetUserFolderRelativePath(string path)
+        {
+            return Server.MapPath("~/Files/" + path);
+        }
+
+        //Получаем папки из директории пользователя
         private IEnumerable<string> GetAllDirectoriesByUserName(string username)
         {
             CheckIfDirectoryWithUserNameExists(username);
-            return Directory.GetDirectories(Server.MapPath("~/Files/" + username));
+            var directoriesPaths = Directory.GetDirectories(Server.MapPath("~/Files/" + username));
+            for (var i = 0; i < directoriesPaths.Length; i++)
+            {
+                directoriesPaths[i] = new FileInfo(directoriesPaths[i]).Name;
+            }
+            return directoriesPaths;
         }
 
+        //Получаем файлы из директории пользователя
+        private IEnumerable<string> GetAllFilesByUserName(string username)
+        {
+            CheckIfDirectoryWithUserNameExists(username);
+            var filesPaths = Directory.GetFiles(Server.MapPath("~/Files/" + username));
+            for (var i = 0; i < filesPaths.Length; i++) 
+            {
+                filesPaths[i] = new FileInfo(filesPaths[i]).Name;
+            }
+            return filesPaths;
+        }
+
+        //Проверяем, существует ли папка пользователя. Если нет, создаем ее
         private void CheckIfDirectoryWithUserNameExists(string username)
         {
             var usersDirectories = Directory.GetDirectories(Server.MapPath("~/Files/"));
