9a5d66585cd771077db18fcad33aa486a453abb2
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-12-20 00:28:31 +0300

    Added Tree project - test project for item rate

---
+++ Tree/Tree/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Tree
+{
+    public class Category
+    {
+        public Category Parent { get; set; }
+        public string Name { get; set; }
+        public int Rate { get; set; }
+    }
+}
---
+++ Tree/Tree/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    class Program
+    {
+        private static List<Category> _categories = new List<Category>();
+
+        static void Main(string[] args)
+        {
+            //Creatin root category and put in into storage
+            var rootCategory = CreateRootCategory();
+            _categories.Add(rootCategory);
+
+            //Start creating categories until user enter 'exit'
+            AddCategory(rootCategory);
+            //Start counting prices of all categories
+            AddWeightOrRate();
+        }
+
+        private static void AddCategory(Category parentCategory)
+        {
+            //Request category name
+            Console.WriteLine("Enter category name or exit to stop");
+            var name = Console.ReadLine();
+
+            //if enter exit break loop
+            if (name != null && !name.Equals("exit"))
+            {
+                var category = new Category
+                {
+                    Name = name,
+                    Parent = parentCategory
+                };
+                _categories.Add(category);
+                AddCategory(category);
+            }
+            //If loop ended then proceed with rate for current category
+            //so call rate method here
+        }
+        
+        private static void AddWeightOrRate(Category category)
+        {
+            Console.WriteLine("Chosee 1-Add Weight or 2-Add Rate");
+            var entry = Console.ReadLine();
+
+            if (entry == "1")
+            {
+                var totalWeight = 100;
+            }
+            else if (entry == "2")
+            {
+            }
+        }
+
+        private static Category CreateRootCategory()
+        {
+            Console.WriteLine("Enter item");
+            var name = Console.ReadLine();
+            return new Category
+            {
+                Name = name,
+                Parent = null,
+                Rate = 0
+            };
+        }
+    }
+}
